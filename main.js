0af45670de12c57d0baa900a72b0b73cb91f9870
Angel Arroyo Herrera <asangekl45y@outlook.com>
2024-02-05 22:32:42 -0600

    termine la entrega

--- main.js
+++ main.js
@@ -1,43 +1,83 @@
 
-function sumar(a, b) {
-    return a + b;
-  }
-  
-  function restar(a, b) {
-    return a - b;
-  }
-  
-  function multiplicar(a, b) {
-    return a * b;
-  }
-  
-  function dividir(a, b) {
-    return b !== 0 ? a / b : "No es posible dividir por cero";
-  }
-  
-  const operacion = prompt("Selecciona una operación (+, -, *, /):");
-  
-  const num1 = parseFloat(prompt("Ingresa el primer número:"));
-  const num2 = parseFloat(prompt("Ingresa el segundo número:"));
-  
-  let resultado;
-  
-  switch (operacion) {
-    case "+":
-      resultado = sumar(num1, num2);
-      break;
-    case "-":
-      resultado = restar(num1, num2);
-      break;
-    case "*":
-      resultado = multiplicar(num1, num2);
-      break;
-    case "/":
-      resultado = dividir(num1, num2);
-      break;
-    default:
-      resultado = "Operación no válida";
-  }
+
+let titulo = document.getElementById("titulo")
+
+titulo.innerHTML = "<h1>PC COMPONENTES</h1> "
+
+let cart = [];
+
+function getCartFromLocalStorage() {
+    const cartFromStorage = localStorage.getItem('cart');
+    if (cartFromStorage) {
+        cart = JSON.parse(cartFromStorage);
+        renderCart();
+    }
+}
+
+function saveCartToLocalStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function addToCart(productName, price) {
+    cart.push({ name: productName, price: price });
+    saveCartToLocalStorage();
+    renderCart();
+}
+
+function removeFromCart(index) {
+    cart.splice(index, 1);
+    saveCartToLocalStorage();
+    renderCart();
+}
+
+function renderCart() {
+    const cartElement = document.getElementById('cart');
+    cartElement.innerHTML = '';
+
+    let total = 0;
+
+    cart.forEach((item, index) => {
+        const itemElement = document.createElement('div');
+        const removeButton = document.createElement('button');
+        removeButton.innerText = 'Eliminar';
+        removeButton.addEventListener('click', () => removeFromCart(index));
+        itemElement.innerText = `${item.name} - $${item.price}`;
+        itemElement.appendChild(removeButton);
+        cartElement.appendChild(itemElement);
+        total += item.price;
+    });
+
+    const totalElement = document.createElement('div');
+    totalElement.innerText = `Total: $${total}`;
+    cartElement.appendChild(totalElement);
+}
+
+getCartFromLocalStorage();
+
+btn.addEventListener("click", function() {
+
+    let nombre= prompt ("Nombre:")
+    let tarea= prompt ("Qué te gustaria?")
+const newcard =document.createElement ("div")
+
+newcard.innerHTML = `
+
+<h2> El usuario:  ${nombre}   </h2>
+<p>    ${tarea} </p>
+`
+container.appendChild (newcard)
+})
+
+
+
+
+
+
+
+
+
+
+
 
 
 
